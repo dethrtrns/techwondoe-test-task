6dc74862207a8d7a06842dc34cf8a57bb68130f1
dethrtrns <rajshekharsingh060593@gmail.com>
2023-05-20 21:59:58 +0530

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <h1>hi</h1>
+    
       <CompanySettings />
     </main>
   );
